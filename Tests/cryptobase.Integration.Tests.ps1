9ec6c51680699755cc87fcd779c9049358e39c42
alain <chadnpc@outlook.com>
2026-05-04 18:32:45 +0200

    Update cryptobase.Integration.Tests.ps1

--- Tests/cryptobase.Integration.Tests.ps1
+++ Tests/cryptobase.Integration.Tests.ps1
@@ -1,4 +1,4 @@
-﻿# verify the interactions and behavior of the module's components when they are integrated together.
+﻿# verify the interactions and behavior of the module's Main class methods.
 Describe "Integration tests: cryptobase" {
   BeforeAll {
     $script:testData = [System.Text.Encoding]::UTF8.GetBytes("hello world 123")
