e19610884fa7613a00cd0137b3ffc4bbfb4c0d46
alain <chadnpc@outlook.com>
2026-05-06 00:47:09 +0200

    Create Get-PsContact.ps1

---
+++ Public/X509/Get-PsContact.ps1
@@ -0,0 +1,22 @@
+﻿function Get-PsContact {
+  # .SYNOPSIS
+  #   Get a list of all contacts you have registered. (Wrapper for [CryptoBase]::GetContact)
+  [CmdletBinding()][OutputType([PSCustomObject[]])]
+  param (
+    [string]$Name = '*'
+  )
+  begin {
+    $c = $null
+  }
+  process {
+    try {
+      $c = [CryptoBase]::GetContact($Name)
+    } catch {
+      # Handle errors during contact retrieval if needed, though class method logs warnings
+      $PSCmdlet.WriteError($_)
+    }
+  }
+  end {
+    return $c
+  }
+}
