31e991ecdf0ee81eebebd07154e97958e201ca8f
alain <chadnpc@outlook.com>
2026-05-06 00:46:43 +0200

    Create Write-TerminatingError.ps1

---
+++ Private/cmdlets/Write-TerminatingError.ps1
@@ -0,0 +1,44 @@
+﻿function Write-TerminatingError {
+  # .SYNOPSIS
+  #   function to throw an errorrecord
+  # .DESCRIPTION
+  #   Used when we don't have built-in ThrowError (ie: $PowerShellversion -lt core-6.1.0-windows)
+  [CmdletBinding()]
+  [OutputType([System.Management.Automation.ErrorRecord])]
+  param (
+    [parameter(Mandatory = $false)]
+    [AllowNull()][System.Management.Automation.PSCmdlet]
+    $Caller = $PSCmdlet,
+
+    [parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()][String]
+    $ExceptionName,
+
+    [parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()][String]
+    $ExceptionMessage,
+
+    [parameter(Mandatory = $false)]
+    [AllowNull()][Object]
+    $ExceptionObject = @{},
+
+    [parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()][String]
+    $ErrorId,
+
+    [parameter(Mandatory = $true)]
+    [ValidateNotNull()][System.Management.Automation.ErrorCategory]
+    $ErrorCategory
+  )
+  process {
+    $exception = New-Object $ExceptionName $ExceptionMessage;
+    $errorRecord = [System.Management.Automation.ErrorRecord]::new($exception, $ErrorId, $ErrorCategory, $ExceptionObject)
+  }
+  end {
+    if ($null -eq $Caller) {
+      $PSCmdlet.ThrowTerminatingError($errorRecord)
+    } else {
+      $Caller.ThrowTerminatingError($errorRecord)
+    }
+  }
+}
