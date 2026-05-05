69cc824d951b649349988f4efc7b346a6818eb40
alain <chadnpc@outlook.com>
2026-05-06 01:29:36 +0200

    Create Get-SecretVault.ps1

---
+++ Public/Vault/Get-SecretVault.ps1
@@ -0,0 +1,24 @@
+function Get-SecretVault {
+  <#
+  .SYNOPSIS
+  Retrieves a secure string secret from the vault.
+
+  .DESCRIPTION
+  Safely fetches a secret corresponding to the provided lookup key from the underlying vault (Windows Credential Manager / cross-platform keychain).
+
+  .PARAMETER Key
+  The unique identifier for the secret.
+
+  .EXAMPLE
+  PS C:\> $secret = Get-SecretVault -Key "API_KEY"
+  #>
+  [CmdletBinding()]
+  [OutputType([string])]
+  param(
+    [Parameter(Mandatory, Position = 0)]
+    [string]$Key
+  )
+  process {
+    [Vault]::GetSecret($Key)
+  }
+}
