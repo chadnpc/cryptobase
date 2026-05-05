4c90b698c7f2db044cc1df83d1ed4365d3e5330d
alain <chadnpc@outlook.com>
2026-05-05 06:27:35 +0200

    Update OpenPgp.psm1

--- Private/OpenPgp.psm1
+++ Private/OpenPgp.psm1
@@ -6,6 +6,9 @@ using namespace System.Text
 using module ./Utilities.psm1
 using module ./Armor.psm1
 using module ./Enums.psm1
+using module ./OpenPgpEnums.psm1
+using module ./OpenPgpCore.psm1
+using module ./OpenPgpPackets.psm1
 
 class OpenPgp : CryptobaseUtils {
   static [string] ArmorMessage([byte[]]$data, [Dictionary[string,string]]$headers) {
