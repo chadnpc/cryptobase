9a200042b5aef70436fd1ac0467fb1247ba3334a
alain <chadnpc@outlook.com>
2026-05-06 01:29:40 +0200

    Create Remove-SecretVault.ps1

---
+++ Public/Vault/Remove-SecretVault.ps1
@@ -0,0 +1,23 @@
+function Remove-SecretVault {
+  <#
+  .SYNOPSIS
+  Removes a stored secret from the vault.
+
+  .DESCRIPTION
+  Deletes an existing credential/secret by its lookup key.
+
+  .PARAMETER Key
+  The unique identifier for the secret to remove.
+
+  .EXAMPLE
+  PS C:\> Remove-SecretVault -Key "API_KEY"
+  #>
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory, Position = 0)]
+    [string]$Key
+  )
+  process {
+    [Vault]::DeleteSecret($Key)
+  }
+}
