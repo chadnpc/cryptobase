afe39b45077968b111ec29f8b16aaf3f3a97c5ec
alain <chadnpc@outlook.com>
2026-05-06 01:29:27 +0200

    Create Get-OtpCode.ps1

---
+++ Public/OTP/Get-OtpCode.ps1
@@ -0,0 +1,25 @@
+function Get-OtpCode {
+  <#
+  .SYNOPSIS
+  Generates a Time-Based One-Time Password (TOTP).
+
+  .DESCRIPTION
+  Generates the standard 6-digit TOTP code for the current time, compliant with RFC 6238.
+  Suitable for multi-factor authentication parity with Google Authenticator.
+
+  .PARAMETER Secret
+  The Base32-encoded secret key assigned to the user.
+
+  .EXAMPLE
+  PS C:\> Get-OtpCode -Secret "JBSWY3DPEHPK3PXP"
+  #>
+  [CmdletBinding()]
+  [OutputType([string])]
+  param(
+    [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
+    [string]$Secret
+  )
+  process {
+    [OTPKIT]::GetTotpCode($Secret)
+  }
+}
